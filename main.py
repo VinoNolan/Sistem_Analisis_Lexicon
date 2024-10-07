edd70cc96d3f3297da473dc52b919301f40e8350
Avin Nolis Ridwan <avinnolis12@gmail.com>
2024-10-07 13:09:54 +0700

    Hapus algoritma login dan admin, menyesuaikan main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from login import login
 from guest_analisis import analisis_sentimen
 from model_kamus import kamus
-from Master_AnalisisSentimen import master
 from cekkalimat_sentimen import cek_kalimatsentimen
 
 # Inisialisasi session state untuk login
@@ -14,8 +12,8 @@ if "logged_in" not in st.session_state:
 with st.sidebar:
     selected = option_menu(
         menu_title="Menu",
-        options=["Analisis Sentimen", "Cek Kalimat Sentimen","Login", "Kamus Inset Lexicon", "Master"],
-        icons=["clipboard-data-fill", "clipboard-data", "door-open-fill", "blockquote-left"],
+        options=["Analisis Sentimen", "Cek Kalimat Sentimen", "Kamus Inset Lexicon"],
+        icons=["clipboard-data-fill", "clipboard-data", "blockquote-left"],
         menu_icon="menu-button-fill",
         default_index=0
     )
@@ -25,17 +23,8 @@ if selected == "Analisis Sentimen":
 
 if selected == "Cek Kalimat Sentimen":
     cek_kalimatsentimen()
-
-# Halaman untuk login
-if selected == "Login":
-    login()
     
 if selected == "Kamus Inset Lexicon":
     kamus()
 
-if selected == "Master":
-    if st.session_state.logged_in:
-        master()
-    else:
-        st.warning("Silahkan login terlebih dahulu lalu akses kembali!")
 
